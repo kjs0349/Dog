31d9956ad643f3d29a443e2018d452ae725e4787
kjs0349 <61913417+kjs0349@users.noreply.github.com>
2020-03-24 11:17:30 +0900

    이미지 변환 토글키

--- action.js
+++ action.js
@@ -1,3 +1,11 @@
-var home {
-    
+function changeImg(age) {
+  if(age.name === 'baby'){
+  var img1 = document.getElementById("img").src = "adultpudle.jpg";
+
+  age.name = 'adult';
+  } else {
+  var img1 = document.getElementById("img").src = "babypudle.jpg";
+
+  age.name = 'baby';
+  }
 }
