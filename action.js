cd52eb48c5fd60535d3e073f67880ff60b5dfe0c
kjs0349 <61913417+kjs0349@users.noreply.github.com>
2020-03-25 09:33:02 +0900

    미니 프로젝트 끝

--- action.js
+++ action.js
@@ -1,10 +1,50 @@
-function changeImg(age) {
+function home() {
+  location.href = "index.html";
+}
+
+function changeImg1(age) {
   if(age.name === 'baby'){
-  var img1 = document.getElementById("img").src = "adultpudle.jpg";
+   document.getElementById("img").src = "adultpudle.jpg";
 
   age.name = 'adult';
   } else {
-  var img1 = document.getElementById("img").src = "babypudle.jpg";
+   document.getElementById("img").src = "babypudle.jpg";
+
+  age.name = 'baby';
+  }
+}
+
+function changeImg2(age) {
+  if(age.name === 'baby'){
+   document.getElementById("img").src = "adultbisyong.jpg";
+
+  age.name = 'adult';
+  } else {
+   document.getElementById("img").src = "babybisyong.jpg";
+
+  age.name = 'baby';
+  }
+}
+
+function changeImg3(age) {
+  if(age.name === 'baby'){
+   document.getElementById("img").src = "adultgoldendudle.jpg";
+
+  age.name = 'adult';
+  } else {
+   document.getElementById("img").src = "babygoldendudle.jpg";
+
+  age.name = 'baby';
+  }
+}
+
+function changeImg4(age) {
+  if(age.name === 'baby'){
+   document.getElementById("img").src = "adultbordercollie.jpg";
+
+  age.name = 'adult';
+  } else {
+   document.getElementById("img").src = "babybordercollie.jpg";
 
   age.name = 'baby';
   }
